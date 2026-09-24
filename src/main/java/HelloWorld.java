28b15f0320a283e6eca11fde3c42dafeec21867b
anwesha-123267 <2022wb86747@wilp.bits-pilani.ac.in>
2026-09-25 05:17:48 +0530

    Update greeting message in HelloWorld class

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello from BITS Jenkins CI Lab!");
+        System.out.println("Hello from BITS Jenkins Continuous Integration Lab!");
     }
 }
